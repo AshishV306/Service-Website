42103ef082cb8f3ea010aaf5b5cf3ab1acf7286b
AshishV306 <110739725+AshishV306@users.noreply.github.com>
2023-07-07 02:45:28 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechnoHub</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/ashish-vispute-885b63251/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/AshishV306" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
